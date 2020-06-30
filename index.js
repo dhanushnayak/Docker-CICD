0cce7bd87ac7b132ee6c84352dd3aea4076d2344
Dhanush Nayak <40546930+dhanushnayak@users.noreply.github.com>
2020-06-30 09:42:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.connect(config.DB, { useNewUrlParser: true }, (err, db) => {
 });
 
 app.get('/', (req, res) => {
-    res.json("I love docker!");
+    res.json("Finally Connected!");
 });
 
 app.listen(PORT, () => {
